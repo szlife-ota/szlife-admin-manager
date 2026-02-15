dfe8bc635e5e287ef81e7880478f11c649badf82
ota <ota@example.com>
2026-02-15 10:25:18 +0000

    Design: Force-injecting Tailwind styles to restore dark mode UI

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          950: '#020617',
+        }
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
